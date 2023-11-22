e6318d69393854fd619fa5e1d9a74924ce16ec7b
Isaias <73768049+meinor25@users.noreply.github.com>
2023-11-22 08:39:22 -0500

    lang base config

---
+++ lang/uiEn.ts
@@ -0,0 +1,10 @@
+export const uiEn = {
+  home: {
+    title: 'Enter your reservation information:',
+    btnText: 'Next',
+    inputErrors: {
+      required: 'This field is required',
+      email: 'Add a valid Email',
+    },
+  },
+};
