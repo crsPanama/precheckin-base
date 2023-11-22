0f8ad1422aac842af48fd3bd7434779d713fd7de
Isaias <73768049+meinor25@users.noreply.github.com>
2023-11-22 08:39:07 -0500

    plugin/modules config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  extends: ['crs_layer'],
   modules: [
-    // Simple usage
     '@nuxtjs/eslint-module',
+    '@nuxtjs/tailwindcss',
+    '@vee-validate/nuxt',
+    'nuxt-directus',
+    '@pinia/nuxt',
+    '@nuxtjs/i18n',
+    '@formkit/nuxt',
   ],
+  i18n: {
+    vueI18n: './i18n.config.ts',
+  },
 });
