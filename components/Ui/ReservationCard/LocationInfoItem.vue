b5e4a319f475b4ded3c1e88276198b69a53a9079
Isaias <73768049+meinor25@users.noreply.github.com>
2023-12-18 14:40:30 -0500

    LocationInfoComponent

---
+++ components/Ui/ReservationCard/LocationInfoItem.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+interface Props {
+  title: string;
+  location: string;
+  date: Date;
+}
+const props = defineProps<Props>();
+
+const formatedDate = computed(() => {
+  return fechaFormat(props.date, true);
+});
+</script>
+<template>
+  <div class="">
+    <h3 class="font-medium">{{ title }}</h3>
+    <p class="text-sm">{{ location }}</p>
+    <p class="text-sm font-medium">{{ formatedDate }}</p>
+  </div>
+</template>
+
+<style scoped></style>
