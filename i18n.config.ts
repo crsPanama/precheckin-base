ea31ac12e5695b4b7e39862367fb1346b3cf9713
Isaias <73768049+meinor25@users.noreply.github.com>
2023-12-18 14:36:44 -0500

    added uiEs to lang

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,12 +1,11 @@
 import { uiEn } from './lang/uiEn';
+import { uiEs } from './lang/uiEs';
 
 export default defineI18nConfig(() => ({
   legacy: false,
   locale: 'en',
   messages: {
     en: uiEn,
-    fr: {
-      welcome: 'Bienvenue',
-    },
+    es: uiEs,
   },
 }));
