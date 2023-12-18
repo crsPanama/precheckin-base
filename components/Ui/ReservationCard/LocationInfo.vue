d6b1d36e2ff3604b409bb13e250f090fe9034f58
Isaias <73768049+meinor25@users.noreply.github.com>
2023-12-18 14:40:20 -0500

    Revie CardLocationIinfo

---
+++ components/Ui/ReservationCard/LocationInfo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    class="border-y-2 border-primary my-2 py-4 flex justify-center text-center gap-x-3"
+  >
+    <UiReservationCardLocationInfoItem
+      title="PICK UP"
+      location="Tocumen (Aeropuerto Internacional)"
+      date="2023 Dec 07, 02:00 PM"
+    />
+    <UiReservationCardLocationInfoItem
+      title="RETURN"
+      location="Tocumen (Aeropuerto Internacional)"
+      :date="new Date('2023 Dec 07, 02:00 PM')"
+    />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
