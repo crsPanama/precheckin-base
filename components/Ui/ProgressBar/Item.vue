bec3d3d8b2b65a66dfcdcf62c19281f2626f6058
Isaias <73768049+meinor25@users.noreply.github.com>
2023-12-18 14:39:49 -0500

    ProgressBar items

---
+++ components/Ui/ProgressBar/Item.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex flex-col items-center relative z-50">
+    <div
+      class="w-8 h-8 rounded-full transition-colors duration-100"
+      :class="[active ? 'bg-primary' : 'bg-slate-300']"
+    ></div>
+    <h3 class="text-xs mt-1 md:text-sm" :class="active && 'font-bold'">
+      {{ text }}
+    </h3>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  hideLine: boolean;
+  text: string;
+  active: boolean;
+}
+
+withDefaults(defineProps<Props>(), {
+  hideLine: false,
+  active: false,
+});
+</script>
