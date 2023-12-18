bd4f13ed47a405d5fda97d6f2ce95c355478693c
Isaias <73768049+meinor25@users.noreply.github.com>
2023-12-18 14:38:54 -0500

    Styling modifications

--- components/Ui/HomeCard.vue
+++ components/Ui/HomeCard.vue
@@ -16,10 +16,10 @@ withDefaults(defineProps<Props>(), {
     class="h-[60vh] md:h-[80vh] flex flex-col justify-center min-w-[320px] bg-no-repeat bg-center bg-cover"
   >
     <article
-      class="mx-4 bg-white rounded p-4 text-center md:flex md:text-left md:py-16 md:max-w-xl md:mx-20 lg:mt-20 lg:mx-0 lg:ml-60"
+      class="mx-4 bg-white rounded p-7 md:p-10 text-center md:flex md:text-left md:py-16 md:max-w-xl md:mx-20 lg:mt-20 lg:mx-0 lg:ml-60"
     >
       <div class="max-md:max-w-[270px] mx-auto">
-        <h1 class="text-xl m-0 pb-4 font-bold lg:text-2xl">
+        <h1 class="text-base md:text-xl m-0 pb-4 font-bold lg:text-2xl">
           {{ $t('home.title') }}
         </h1>
 
