851027e13f718f10b2fa7cca82a7bf0c0f69ec8e
Isaias <73768049+meinor25@users.noreply.github.com>
2024-01-05 15:45:51 -0500

    dynamic reservation date

--- components/Ui/ReservationCard/LocationInfo.vue
+++ components/Ui/ReservationCard/LocationInfo.vue
@@ -1,3 +1,6 @@
+<script setup lang="ts">
+const { getRebuildedDate } = usePrecheckin();
+</script>
 <template>
   <div
     class="border-y-2 border-primary my-2 py-4 flex justify-center text-center gap-x-3"
@@ -5,16 +8,14 @@
     <UiReservationCardLocationInfoItem
       title="PICK UP"
       location="Tocumen (Aeropuerto Internacional)"
-      date="2023 Dec 07, 02:00 PM"
+      :date="getRebuildedDate.pickupDate"
     />
     <UiReservationCardLocationInfoItem
       title="RETURN"
       location="Tocumen (Aeropuerto Internacional)"
-      :date="new Date('2023 Dec 07, 02:00 PM')"
+      :date="getRebuildedDate.dueDate"
     />
   </div>
 </template>
-
-<script setup lang="ts"></script>
 
 <style scoped></style>
