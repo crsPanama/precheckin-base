dce728bc6d1dea9f40aa44d7bfdc14efefd8597b
Isaias <73768049+meinor25@users.noreply.github.com>
2024-01-23 16:41:39 -0500

    EraSelection component

---
+++ components/Ui/Era/Selection.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const ERA_VALUE = 3.99;
+const ERA_DESCRIPTION =
+  'ROAD ASSISTANCE (ERA) This coverage offers THE LESSEE the following roadside assistance services free of charge: Loss of car key, towing service, tire replacement, assistance when running out of fuel or battery. The response time by THE LESSOR is subject to the day, time and location of the incident.';
+const { setEra, state } = usePrecheckin();
+
+const checkEraIsSelected = computed(() => {
+  return state.value.prices.Era === ERA_VALUE;
+});
+
+const handleEraSelection = () => {
+  if (checkEraIsSelected.value) {
+    return setEra(0);
+  }
+  setEra(ERA_VALUE);
+};
+</script>
+<template>
+  <UiChargeContainer title="Optional Coverages">
+    <UiChargeItem
+      title="Roadside Assistance (ERA)"
+      :switch-active="checkEraIsSelected"
+      :description="ERA_DESCRIPTION"
+      :price="ERA_VALUE"
+      border-color="#0365C7"
+      @switch-checked="handleEraSelection()"
+    />
+  </UiChargeContainer>
+</template>
+
+<style scoped></style>
