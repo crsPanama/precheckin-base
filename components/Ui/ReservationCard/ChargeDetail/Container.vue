b95234efc2e2ed6c65dbc9bccd58308c2bcdec9b
Isaias <73768049+meinor25@users.noreply.github.com>
2024-01-23 16:42:09 -0500

    ChargeDetail ui container/ item

---
+++ components/Ui/ReservationCard/ChargeDetail/Container.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{ title: string }>();
+const { calcuatePrices } = usePrices();
+</script>
+<template>
+  <div class="py-2">
+    <h4 class="font-bold md:text-lg">{{ title }}</h4>
+
+    <slot name="charge-item" :prices="calcuatePrices" />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ components/Ui/ReservationCard/ChargeDetail/Item.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps<{ title: string; price: number }>();
+</script>
+<template>
+  <div class="flex my-1 justify-between text-sm md:text-base">
+    <p class="w-3/4">{{ title }}</p>
+    <p>${{ price }}</p>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
