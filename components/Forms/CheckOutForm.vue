5de30c1e68bbc110378ff875abc84f5f5f27fdf2
Isaias <73768049+meinor25@users.noreply.github.com>
2024-01-24 16:50:35 -0500

    Refactor field creation

--- components/Forms/CheckOutForm.vue
+++ components/Forms/CheckOutForm.vue
@@ -3,22 +3,29 @@ import * as yup from 'yup';
 /* This regular expression validates that the card expiration date is in the correct format MM/YY */
 const expDateRegex = /^(0[1-9]|1[0-2])\/(\d{2})$/;
 
-const { useFieldModel, handleSubmit, errors } = useForm({
+withDefaults(defineProps<{ primaryColor: string }>(), {
+  primaryColor: 'black',
+});
+const emit = defineEmits(['submit-payment']);
+
+const { handleSubmit, errors } = useForm({
+  validateOnMount: true,
   validationSchema: {
     ccnumber: yup.number().required(),
     ccexp: yup.string().matches(expDateRegex).required(),
     cvv: yup.number().required(),
   },
 });
-const emit = defineEmits(['submit-payment']);
-const [ccnumber, ccexp, cvv] = useFieldModel(['ccnumber', 'ccexp', 'cvv']);
+const { value: ccnumber, errorMessage: ccnumberError } = useField('ccnumber');
+const { value: ccexp, errorMessage: ccexpError } = useField('ccexp');
+const { value: cvv, errorMessage: cvvError } = useField('cvv');
+
+const isFormInvalid = computed(() => {
+  return Object.keys(errors.value).length > 0;
+});
 
 const onSubmit = handleSubmit((cardValues) => {
   emit('submit-payment', cardValues);
-});
-
-withDefaults(defineProps<{ primaryColor: string }>(), {
-  primaryColor: 'black',
 });
 </script>
 <template>
@@ -30,26 +37,26 @@ withDefaults(defineProps<{ primaryColor: string }>(), {
         name="card-number"
         placeholder="4111 1111 1111 1111"
         :focus-color="primaryColor"
-        :error-message="errors.ccnumber && 'Add a valid Card Number'"
+        :error-message="ccnumberError && 'Add a valid Card Number'"
       />
     </section>
-    <section class="flex gap-x-5">
-      <div class="w-1/2">
+    <section class="md:flex gap-x-5">
+      <div class="max-md:pb-4 md:w-1/2">
         <label for="card-exp" class="pb-2"> Expiration Date </label>
         <UiCustomInput
           v-model="ccexp"
           placeholder="10/12"
           name="card-exp"
           :focus-color="primaryColor"
-          :error-message="errors.ccexp && 'Add a valid expiration date'"
+          :error-message="ccexpError && 'Add a valid expiration date'"
         />
       </div>
-      <div class="w-1/2">
+      <div class="md:w-1/2">
         <label for="card-cvv" class="pb-2"> CVV </label>
         <UiCustomInput
           v-model="cvv"
           placeholder="999"
-          :error-message="errors.cvv && 'Add a valid CVV number'"
+          :error-message="cvvError && 'Add a valid CVV number'"
           name="card-cvv"
           :focus-color="primaryColor"
         />
@@ -59,11 +66,12 @@ withDefaults(defineProps<{ primaryColor: string }>(), {
     <section class="mt-5">
       <UiBaseButton
         type="submit"
-        class="bg-primary p-3"
+        class="bg-primary p-3 disabled:bg-primary/75"
         width="100%"
+        :disabled="isFormInvalid"
         light-text
       >
-        Process Payment
+        Pay
       </UiBaseButton>
     </section>
   </form>
