f735cf5617726940f2e68f964378917de0ca52d2
Isaias <73768049+meinor25@users.noreply.github.com>
2024-01-26 10:20:59 -0500

    added PayPal payment logic

--- composables/usePayment.ts
+++ composables/usePayment.ts
@@ -1,6 +1,8 @@
 import type { Card } from 'crs_layer/types/card';
+import type { PaypalInfo } from 'crs_layer/types/paypalInfo';
 
 export const usePayment = (
+  paypalInfo: Pick<PaypalInfo, 'custom_id' | 'soft_descriptor'>,
   cardPaymentSecurityKey?: string,
   cardPaymentUrl?: string,
   paypalSecurityKey?: string
@@ -14,6 +16,12 @@ export const usePayment = (
     paymentProcess,
     setPaymentErrorMessage,
   } = useCardPayment(cardPaymentSecurityKey, cardPaymentUrl);
+
+  const { generatePaypalBtn, success } = usePaypal();
+  const { state, getReservationUpdateItems } = usePrecheckin();
+  const { selectedCar } = useCar();
+  const { selectedCoverage } = useCoverages();
+  const { updateReservationData } = useReservation('prechecking');
 
   const processCardPayment = async (cardValues: Card, params: {}) => {
     card.value = {
@@ -32,11 +40,35 @@ export const usePayment = (
       }, 5000);
     }
   };
+  watch(success, async () => {
+    if (success.value) {
+      updateReservationData({
+        ...getReservationUpdateItems.value,
+        status: 'Cancelado',
+        tipo_pago: 'PayPal',
+      });
+    }
+    setTimeout(async () => {
+      await navigateTo('success');
+    }, 4000);
+  });
+  onMounted(async () => {
+    await generatePaypalBtn(paypalSecurityKey!, {
+      amount: {
+        value: String(state.value.prices.Total),
+      },
+      invoice_id: state.value.reservation.Res,
+      description: `Renta de auto ${selectedCar.value.modelo} ${selectedCar.value.marca} - ${selectedCoverage.value.nombre} `,
+      custom_id: paypalInfo.custom_id + state.value.reservation.Res,
+      soft_descriptor: paypalInfo.soft_descriptor,
+    });
+  });
 
   return {
     cardPaymentErrorMessage: paymentErrorMessage,
     cardPaymentFullfiled: paymentFullfiled,
     isCardPaymentProcessing: isPaymentProcessing,
+    paypalPaymentFullfiled: success,
     processCardPayment,
   };
 };
