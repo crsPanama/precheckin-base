92a8872115ea22b16e26ad778f84fd5c27645626
Isaias <73768049+meinor25@users.noreply.github.com>
2024-01-30 10:25:01 -0500

    useReservation - fetchReservation funciton

--- composables/useReservation.ts
+++ composables/useReservation.ts
@@ -1,6 +1,10 @@
-import type { Coverage } from 'crs_layer/types/coverage';
-import type { Extra } from 'crs_layer/types/extra';
-import type { PricingInfo, ReservationInfo } from './../types/precheckin.d';
+import type {
+  Brands,
+  ClientInfo,
+  EstimatedTotal,
+  PricingInfo,
+  ReservationInfo,
+} from './../types/precheckin.d';
 import type { ReservationUpdateItems } from '../types/precheckin';
 // type Item = {
 //   prices: PricingInfo;
@@ -13,6 +17,29 @@ export const useReservation = (DB_COLLECTION: string) => {
   const isRequestProcessing = ref<boolean>(false);
   const requestError = ref<string>('');
   const { updateItem } = useDirectusFetch();
+  const { fetchItems } = useDirectusFetch();
+
+  const fetchReservation = async (
+    Res: string,
+    RentersEmail: string,
+    brand: Brands
+  ) => {
+    const filters: Pick<ClientInfo, 'Renters_Email'> &
+      Pick<ReservationInfo, 'Res' | 'marca'> = {
+      Res,
+      marca: brand,
+      Renters_Email: RentersEmail,
+    };
+    const { data, error } = await fetchItems<
+      ClientInfo & ReservationInfo & EstimatedTotal
+    >(DB_COLLECTION, {
+      filter: filters,
+    });
+    return {
+      data: data && data[0],
+      error,
+    };
+  };
 
   const updateReservationData = async (item: ReservationUpdateItems) => {
     isRequestProcessing.value = true;
@@ -28,5 +55,6 @@ export const useReservation = (DB_COLLECTION: string) => {
 
   return {
     updateReservationData,
+    fetchReservation,
   };
 };
